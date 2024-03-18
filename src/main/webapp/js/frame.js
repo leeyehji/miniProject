2c20f0b59c68661057245d097568e07a13861e5e
leeyehji <lamp0525@naver.com>
2024-03-18 22:11:15 +0900

    width size increase and add site map

--- src/main/webapp/js/frame.js
+++ src/main/webapp/js/frame.js
@@ -1,54 +1,60 @@
 /*header*/
-    /*main menu nav*/ 
-    $(function () {
-        $(".menu-content").hide();
-    
-        $(".nav").hover(
-          function () {
-            $(this).find(".menu-content").stop(true, true).slideDown(300);
-          },
-          function () {
-            $(this).find(".menu-content").stop(true, true).slideUp(300);
-          }
-        );
-      });
-    
-      var mybutton = document.getElementById("myButton");
-    
-        window.onscroll = function() {scrollFunction()};
-    
-        function scrollFunction() {
-            mybutton.style.display = "block";
-        }
-    
-        function topFunction() {
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0;
-        }
-    
-        
-        
-/*contnent*/   
-    /*nav*/
+/*main menu nav*/
+$(function() {
+	$(".menu-content").hide();
 
-
-$(function () {
-  $(".nav-content").hide();
-  
-  $("#nadeuliNav > li").click(function(event) { // event 매개변수 추가
-    $(this).children(".nav-content").slideToggle(300);
-    event.stopPropagation();
-  });
-  $(".nav-content").click(function(event) {
-    event.stopPropagation();
-  });
+	$(".nav").hover(
+		function() {
+			$(this).find(".menu-content").stop(true, true).slideDown(300);
+		},
+		function() {
+			$(this).find(".menu-content").stop(true, true).slideUp(300);
+		}
+	);
 });
 
-    /*page content*/
-    
+var mybutton = document.getElementById("myButton");
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+	mybutton.style.display = "block";
+}
+
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+
+
+
+/*contnent*/
+/*nav*/
+
+
+$(function() {
+	$(".nav-content").hide();
+
+	$("#nadeuliNav > li").click(function(event) { // event 매개변수 추가
+		$(this).children(".nav-content").slideToggle(300);
+		event.stopPropagation();
+	});
+	$(".nav-content").click(function(event) {
+		event.stopPropagation();
+	});
+});
+
+/*page content*/
+
 
 /*footer*/
-    /*site map*/
-
-    /*main footer*/
-    
+/*site map*/
+function goToSite() {
+	var site = document.getElementById('site-select').value;
+	if (site) {
+		window.location = site;
+	} else {
+		alert('사이트를 선택해주세요.');
+	}
+}
+/*main footer*/
