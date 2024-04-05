df305aab6bff2ace5eaba7fc093c3856f62fb21e
jyc961020 <jyc961020@naver.com>
2024-04-05 17:38:16 +0900

    도메인형식

--- src/main/java/com/Spring/miniprojectMaven/HomeController.java
+++ src/main/java/com/Spring/miniprojectMaven/HomeController.java
@@ -1,6 +1,7 @@
 package com.Spring.miniprojectMaven;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,11 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Handles requests for the application home page.
  */
 @Controller
+@Slf4j
 public class HomeController {
 	
 	@RequestMapping(value = "/")
 	public String home() {
-		
+
+
 		return "mainpage";
 	}
 	
